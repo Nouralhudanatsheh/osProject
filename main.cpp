b56b9f31d8d54404ef86093895d895a93af71300
Nour AlHuda Natsheh <nouralhudanatsheh@Nouralhudas-MacBook-Air.local>
2024-05-13 23:14:45 +0300

    RR first implementation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 
 #include <algorithm>
+#include <cstddef>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <queue>
 #include <vector>
 using namespace std;
@@ -19,14 +21,28 @@ struct Process {
 
 class schedulingAlg {
 private:
-  int contextSwitch;
-  int quantum;
+  float contextSwitch;
+  float quantum;
   vector<Process> processes;
-  struct com {
+  struct compareFCFS {
     bool operator()(Process p1, Process p2) {
+
       return p1.arrivalTime > p2.arrivalTime;
     }
   };
+  struct compareSRTat {
+    bool operator()(Process p1, Process p2) {
+      if (p1.arrivalTime == p2.arrivalTime)
+        return p1.cpuBursts > p2.cpuBursts;
+      return p1.arrivalTime > p2.arrivalTime;
+    }
+  };
+  struct compareSRTrt {
+    bool operator()(Process p1, Process p2) {
+      return p1.remainingBursts > p2.remainingBursts;
+    }
+  };
+
   void ganttChartandDetails(vector<Process> processes, float cpuTime,
                             float idleTime) {
 
@@ -75,8 +91,11 @@ private:
       }
     }
     cout << processes[processes.size() - 1].finishTime << endl;
-    cout << "\nCPU utilization = (" << cpuTime << " - " << idleTime << ") / "
-         << cpuTime << " * 100% = " << (cpuTime - idleTime) * 100 / cpuTime
+  }
+  void processesDetails(vector<Process> processes, float cpuTime,
+                        float idleTime) {
+    cout << "\nCPU utilization = ((" << cpuTime << " - " << idleTime << ") / "
+         << cpuTime << ") * 100% = " << (cpuTime - idleTime) * 100 / cpuTime
          << "%" << endl;
     cout << "\nDetails of each process:\n";
     for (int i = 0; i < processes.size(); i++) {
@@ -88,6 +107,29 @@ private:
 
            << " \n waiting time: " << processes[i].waitingTime << endl
            << endl;
+    }
+  }
+
+  void atSorting() {
+    priority_queue<Process, vector<Process>, compareFCFS> FCFS;
+    for (int i = 0; i < processes.size(); i++) {
+      FCFS.push(processes[i]);
+    }
+    processes.clear();
+    for (int i = 0; !FCFS.empty(); i++) {
+      processes.push_back(FCFS.top());
+      FCFS.pop();
+    }
+  }
+  void atSortingRT() {
+    priority_queue<Process, vector<Process>, compareSRTat> FCFS;
+    for (int i = 0; i < processes.size(); i++) {
+      FCFS.push(processes[i]);
+    }
+    processes.clear();
+    for (int i = 0; !FCFS.empty(); i++) {
+      processes.push_back(FCFS.top());
+      FCFS.pop();
     }
   }
 
@@ -116,57 +158,161 @@ public:
 
   void FCFS() {
     if (!processes.empty()) {
-      priority_queue<Process, vector<Process>, com> FCFS;
-      for (int i = 0; i < processes.size(); i++) {
-        FCFS.push(processes[i]);
-      }
 
-      vector<Process> sortedProcesses;
-      for (int i = 0; !FCFS.empty(); i++) {
-        sortedProcesses.push_back(FCFS.top());
-        FCFS.pop();
-      }
-      float cpuTime = sortedProcesses[0].finishTime =
-          sortedProcesses[0].arrivalTime + sortedProcesses[0].cpuBursts;
-      float idleTime = sortedProcesses[0].arrivalTime;
-      sortedProcesses[0].startTime = sortedProcesses[0].arrivalTime;
-      sortedProcesses[0].turnanroundTime =
-          cpuTime - sortedProcesses[0].arrivalTime;
+      atSorting();
+      float cpuTime = processes[0].finishTime =
+          processes[0].arrivalTime + processes[0].cpuBursts;
+      float idleTime = processes[0].arrivalTime;
+      processes[0].startTime = processes[0].arrivalTime;
+      processes[0].turnanroundTime = cpuTime - processes[0].arrivalTime;
 
       for (int i = 1; i < processes.size(); i++) {
 
-        if (sortedProcesses[i].arrivalTime > cpuTime &&
-            sortedProcesses[i].arrivalTime >= cpuTime + contextSwitch) {
-          idleTime += sortedProcesses[i].arrivalTime - cpuTime;
-          cpuTime += sortedProcesses[i].arrivalTime - cpuTime;
+        if (processes[i].arrivalTime > cpuTime &&
+            processes[i].arrivalTime >= cpuTime + contextSwitch) {
+          idleTime += processes[i].arrivalTime - cpuTime;
+          cpuTime += processes[i].arrivalTime - cpuTime;
         } else {
           cpuTime += contextSwitch;
           idleTime += contextSwitch;
         }
 
-        sortedProcesses[i].startTime = cpuTime;
-        sortedProcesses[i].finishTime =
-            sortedProcesses[i].startTime + sortedProcesses[i].cpuBursts;
-        sortedProcesses[i].turnanroundTime =
-            sortedProcesses[i].finishTime - sortedProcesses[i].arrivalTime;
-        sortedProcesses[i].waitingTime =
-            sortedProcesses[i].startTime - sortedProcesses[i].arrivalTime;
-        cpuTime += sortedProcesses[i].cpuBursts;
+        processes[i].startTime = cpuTime;
+        processes[i].finishTime =
+            processes[i].startTime + processes[i].cpuBursts;
+        processes[i].turnanroundTime =
+            processes[i].finishTime - processes[i].arrivalTime;
+        processes[i].waitingTime =
+            processes[i].startTime - processes[i].arrivalTime;
+        cpuTime += processes[i].cpuBursts;
       }
       cout << "First Come First Served Algorithm\n\n";
-      ganttChartandDetails(sortedProcesses, cpuTime, idleTime);
-      // cout << " cpu time = " << cpuTime << " idle time = " << idleTime
-      //      << " cpu utilization time =  " << (cpuTime - idleTime) / cpuTime;
-    } else {
+      ganttChartandDetails(processes, cpuTime, idleTime);
+    } else
       cout << "no processes found";
-    }
   }
 
-  void SRT() {}
-};
+  void SRT() {
+    if (!processes.empty()) {
+      atSortingRT();
+
+      for (int i = 0; i < processes.size(); i++)
+        processes[i].remainingBursts = processes[i].cpuBursts;
+
+      priority_queue<Process, vector<Process>, compareSRTat> SRTQ;
+
+      int cpuTime = processes[0].arrivalTime;
+      int idleTime = processes[0].arrivalTime;
+      Process currentProcess = processes[0];
+      vector<Process> SRTV;
+      SRTV.push_back(processes[0]);
+      vector<Process>::iterator it = processes.begin();
+
+      cout << (*(5 + it)).arrivalTime << endl;
+      cout << (*it).arrivalTime;
+      // while (true) {
+
+      //   if (cpuTime == (*it++).arrivalTime &&
+      //       SRTQ.top().remainingBursts < currentProcess.remainingBursts)
+
+      //   {
+      //     currentProcess.finishTime = cpuTime;
+      //     currentProcess.waitingTime =
+      //         currentProcess.startTime - currentProcess.finishTime;
+      //     SRTV.push_back(currentProcess);
+      //     SRTQ.push(currentProcess);
+      //     cpuTime += contextSwitch;
+      //     idleTime += contextSwitch;
+      //     currentProcess = SRTQ.top();
+      //     currentProcess.startTime = cpuTime;
+      //     SRTQ.pop();
+      //   } else if (currentProcess.remainingBursts) {
+      //     currentProcess.remainingBursts--;
+      //     cpuTime++;
+      //   } else if (!SRTQ.empty())
+      //     currentProcess = SRTQ.top();
+      //   else
+      //     break;
+      //   ;
+      // }
+
+    }
+
+    else
+      cout << "no processes found";
+  }
+  void RR() {
+    if (!processes.empty()) {
+      atSorting();
+      const int n = processes.size();
+      int arr[n];
+      for (int i = 0; i < n; i++)
+        arr[i] = ceil(processes[i].cpuBursts / quantum);
+
+      for (int i = 0; i < processes.size(); i++)
+        processes[i].remainingBursts = processes[i].cpuBursts;
+      Process *currentProcess = &processes[0];
+      float cpuTime = processes[0].arrivalTime;
+      float idleTime = processes[0].arrivalTime;
+      vector<Process> RR;
+      int tempST;
+      int flag = 0;
+      for (int i = 0; flag != n - 1;) {
+        flag = 0;
+        if (arr[i] > 0) {
+          if (ceil(processes[i].cpuBursts / quantum) == arr[i]) {
+            (*currentProcess).startTime = tempST = cpuTime;
+            (*currentProcess).waitingTime +=
+                (*currentProcess).startTime - (*currentProcess).arrivalTime;
+          } else {
+            tempST = cpuTime;
+            (*currentProcess).waitingTime +=
+                (*currentProcess).startTime - (*currentProcess).finishTime;
+          }
+
+          if ((*currentProcess).remainingBursts < quantum)
+            cpuTime += (*currentProcess).remainingBursts;
+          else
+            cpuTime += quantum;
+          (*currentProcess).finishTime = cpuTime;
+          if (arr[i] == 1)
+            (*currentProcess).turnanroundTime +=
+                (*currentProcess).finishTime - (*currentProcess).arrivalTime;
+          if ((*currentProcess).remainingBursts >= quantum)
+            (*currentProcess).remainingBursts -= quantum;
+          else
+            (*currentProcess).remainingBursts = 0;
+          RR.push_back(*currentProcess);
+          arr[i]--;
+        }
+        if (1 + currentProcess != NULL)
+          ++currentProcess;
+        else
+          currentProcess = &processes[0];
+        if (i < n)
+          i++;
+        else
+          i = 0;
+
+        for (int j = 0; j < n; j++) {
+          if (arr[j] == 0)
+            flag++;
+        }
+      }
+
+      ganttChartandDetails(RR, cpuTime, idleTime);
+      processesDetails(processes, cpuTime, idleTime);
+
+    } else
+      cout << "no processes found";
+  }
+}
+
+;
 
 int main() {
   schedulingAlg test;
-  test.FCFS();
+  test.RR();
+
   return 0;
 }
